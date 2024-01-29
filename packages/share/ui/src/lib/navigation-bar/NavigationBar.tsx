cadddb6e888c30ee6bfa9d8c7ee2c9bc782fa5b8
Serey Vatanak <45728734+vatanak10@users.noreply.github.com>
2024-01-29 20:34:45 +0700

    :construction: improved navigation bar

--- packages/share/ui/src/lib/navigation-bar/NavigationBar.tsx
+++ packages/share/ui/src/lib/navigation-bar/NavigationBar.tsx
@@ -1,5 +1,54 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
 export function NavigationBar() {
-  return <div>this is nav bar</div>;
+  const menu = [
+    {
+      name: 'Home',
+      url: '/',
+    },
+    {
+      name: 'Work',
+      url: '/work',
+    },
+    {
+      name: 'Blog',
+      url: '/blog',
+    },
+    {
+      name: 'Sketch',
+      url: '/sketch',
+    },
+  ];
+
+  return (
+    <div className="px-12 bg-black">
+      <div className="flex items-center justify-between">
+        <Link href="/">
+          <Image
+            src="/svg/HomeLogo.svg"
+            alt="Logo"
+            width={70}
+            height={50}
+            className="pt-4"
+          />
+        </Link>
+
+        <div className="flex items-center">
+          {menu.map((item, index) => (
+            <Link href={item.url}>
+              <div
+                key={index}
+                className="px-4 py-2 text-white uppercase hover:bg-[#2f2f2f] cursor-pointer rounded-sm transition-all duration-300"
+              >
+                {item.name}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NavigationBar;
