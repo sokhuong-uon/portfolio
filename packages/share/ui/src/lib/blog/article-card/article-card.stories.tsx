bbbb1a86f8154537a1e7814a2642df1cb7681749
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-02-07 13:45:44 +0700

    docs: remove comment

--- packages/share/ui/src/lib/blog/article-card/article-card.stories.tsx
+++ packages/share/ui/src/lib/blog/article-card/article-card.stories.tsx
@@ -9,11 +9,6 @@ const meta: Meta<typeof ArticleCard> = {
 export default meta;
 type Story = StoryObj<typeof ArticleCard>;
 
-/*
- *👇 Render functions are a framework specific feature to allow you control on how the component renders.
- * See https://storybook.js.org/docs/api/csf
- * to learn how to use render functions.
- */
 export const Primary: Story = {
   args: {
     title: 'This is Title',
