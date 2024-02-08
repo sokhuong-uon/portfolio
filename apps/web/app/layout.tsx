e3cf826868a2e6c6ea975abf9880ed787567a3c5
Sokhuong Uon <sokhuong.usk@gmail.com>
2024-02-08 16:39:14 +0700

    fix: html and body size

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -19,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={prataFont.className}>
-      <body>
+    <html lang="en" className={`${prataFont.className} h-full`}>
+      <body className="w-full h-full">
         <NavigationBar />
         {children}
       </body>
