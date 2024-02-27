3c922df1f5f6f54a5efcdfb77649fc45e9821a32
Serey Vatanak <45728734+vatanak10@users.noreply.github.com>
2024-02-27 20:51:15 +0700

    feat: update first item of blog list

--- apps/web/app/blog/page.tsx
+++ apps/web/app/blog/page.tsx
@@ -2,9 +2,16 @@ import { BlogCard } from '@portfolio/share-ui';
 
 export default async function Index() {
   return (
-    <div className="container mx-auto text-2xl text-rose-600">
+    <div className="container mx-auto ">
       <div className="grid grid-cols-3 gap-4 mt-6">
-        <span>Welcome to blog!</span>
+        <div className="p-4">
+          <div className="text-xl">Welcome to blog!</div>
+          <div className="mt-2 text-sm">
+            Capturing highlight. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit.
+          </div>
+        </div>
+
         {Array.from({ length: 5 }).map((_, i) => (
           <BlogCard
             key={i}
